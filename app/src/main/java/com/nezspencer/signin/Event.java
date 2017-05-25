d1e2f6f5c1f8f09babcb01b6e6d49c0e4bea54a6
NezSpencer <nuhiara@gmail.com>
2017-05-25 14:32:25 +0100

    hook up recyclerview to adapter

--- app/src/main/java/com/nezspencer/signin/Event.java
+++ app/src/main/java/com/nezspencer/signin/Event.java
@@ -13,13 +13,17 @@ public class Event extends AppCompatActivity {
     private RecyclerView eventRecycler;
     public static ArrayList<EventPojo> eventList;
     private FloatingActionButton fabAdd;
+    private EventsAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
         eventList = new ArrayList<>();
+        adapter = new EventsAdapter(this, eventList);
         eventRecycler = (RecyclerView) findViewById(R.id.rv_event);
+        eventRecycler.setAdapter(adapter);
+
         fabAdd = (FloatingActionButton) findViewById(R.id.fab_add);
 
         fabAdd.setOnClickListener(new View.OnClickListener() {
