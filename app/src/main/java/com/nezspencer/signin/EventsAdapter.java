753e4a2555f6f65ccdcc7ecd3820b2cf9ecf6980
NezSpencer <nuhiara@gmail.com>
2017-05-25 14:09:47 +0100

    first commit

--- app/src/main/java/com/nezspencer/signin/EventsAdapter.java
+++ app/src/main/java/com/nezspencer/signin/EventsAdapter.java
@@ -2,7 +2,10 @@ package com.nezspencer.signin;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -10,7 +13,7 @@ import java.util.ArrayList;
  * Created by nezspencer on 5/25/17.
  */
 
-public class EventsAdapter extends RecyclerView.Adapter {
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.Holder> {
 
     private ArrayList<EventPojo> events;
     private Context context;
@@ -20,18 +23,39 @@ public class EventsAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_event_item,
+                parent,false);
+        return new Holder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(Holder holder, int position) {
 
-
+        EventPojo pojo = events.get(position);
+        holder.titleText.setText(pojo.getTitle());
+        holder.summaryText.setText(pojo.getSummary());
+        holder.dateText.setText(pojo.getDate());
     }
 
     @Override
     public int getItemCount() {
         return events.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder{
+
+        private TextView titleText;
+        private TextView summaryText;
+        private TextView dateText;
+
+        public Holder(View itemView) {
+            super(itemView);
+
+            titleText = (TextView) itemView.findViewById(R.id.tv_title);
+            summaryText = (TextView) itemView.findViewById(R.id.tv_summary);
+            dateText = (TextView) itemView.findViewById(R.id.tv_date);
+
+        }
     }
 }
